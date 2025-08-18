308008f3f8f8c05180e0f72ab1d977069234961f
Michael Smith <michael.smith6@mail.mcgill.ca>
2025-08-18 17:19:30 -0400

    sync pip fix with base repo + include server and full docker images in script

--- compile_cuda11.sh
+++ compile_cuda11.sh
@@ -2,3 +2,6 @@
 
 docker build -t mdsmithcim/llama.cpp:cuda11-full --target full -f .devops/cuda-11.Dockerfile .
 docker push mdsmithcim/llama.cpp:cuda11-full
+
+docker build -t mdsmithcim/llama.cpp:cuda11-server --target server -f .devops/cuda-11.Dockerfile .
+docker push mdsmithcim/llama.cpp:cuda11-server
