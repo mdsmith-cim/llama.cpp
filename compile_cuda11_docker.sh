d421e716037d14d09ce1e3ad5e72b7eacef39d34
Michael Smith <michael.smith6@mail.mcgill.ca>
2025-11-27 12:00:32 -0500

    remove seperate cuda 11 docker file, replace with proper argument usage

--- compile_cuda11_docker.sh
+++ compile_cuda11_docker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-docker build -t mdsmithcim/llama.cpp:cuda11-full --target full -f .devops/cuda-11.Dockerfile .
+# K40 CUDA architecture = 35
+# Max CUDA on K40 is 11
+
+docker build -t mdsmithcim/llama.cpp:cuda11-full --target full --build-arg CUDA_VERSION=11.8.0 --build-arg CUDA_DOCKER_ARCH=35 -f .devops/cuda.Dockerfile .
 docker push mdsmithcim/llama.cpp:cuda11-full
 
-docker build -t mdsmithcim/llama.cpp:cuda11-server --target server -f .devops/cuda-11.Dockerfile .
+docker build -t mdsmithcim/llama.cpp:cuda11-server --target server --build-arg CUDA_VERSION=11.8.0 --build-arg CUDA_DOCKER_ARCH=35 -f .devops/cuda.Dockerfile .
 docker push mdsmithcim/llama.cpp:cuda11-server
